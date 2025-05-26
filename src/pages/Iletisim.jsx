cc41260ad3c3e7509d58d2127b8bf51d90a73131
Emir Ercan <emirercan3459@gmail.com>
2025-05-27 00:48:37 +0300

    x

--- src/pages/Iletisim.jsx
+++ src/pages/Iletisim.jsx
@@ -72,15 +72,15 @@ function Iletisim() {
               <form onSubmit={handleSubmit}>
                 <div className="mb-3">
                   <label htmlFor="adSoyad" className="form-label">Ad Soyad</label>
-                  <input type="text" className="form-control" id="adSoyad" name="adSoyad" value={formData.adSoyad} onChange={handleChange} required/>
+                  <input type="text" className="form-control" id="adSoyad" name="adSoyad" value={formData.adSoyad} required/>
                 </div>
                 <div className="mb-3">
                   <label htmlFor="eposta" className="form-label">E-posta Adresi</label>
-                  <input type="email" className="form-control" id="eposta" name="eposta" value={formData.eposta} onChange={handleChange} required />
+                  <input type="email" className="form-control" id="eposta" name="eposta" value={formData.eposta}required />
                 </div>
                 <div className="mb-3">
                   <label htmlFor="mesaj" className="form-label">Mesaj</label>
-                  <textarea className="form-control" id="mesaj" name="mesaj" rows="3" value={formData.mesaj} onChange={handleChange} required></textarea>
+                  <textarea className="form-control" id="mesaj" name="mesaj" rows="3" value={formData.mesaj} required></textarea>
                 </div>
                 <button type="submit" className="btn btn-primary">Gönder</button>
               </form>
