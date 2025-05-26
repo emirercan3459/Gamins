f6bded7e604d7322ae43c7c654bde380e470eaab
Emir Ercan <emirercan3459@gmail.com>
2025-05-27 01:07:21 +0300

    u

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { React } from 'react'
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Home from "./pages/Anasayfa";
 import Hakkimizda from "./pages/Hakkimizda";
 import Haberler from "./pages/Haberler";
