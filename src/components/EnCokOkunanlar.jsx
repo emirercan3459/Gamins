5bbec9afcbd9049b4f2dde1ba9af01f6720a9d5c
Emir Ercan <emirercan3459@gmail.com>
2025-05-27 15:46:44 +0300

    u

--- src/components/EnCokOkunanlar.jsx
+++ src/components/EnCokOkunanlar.jsx
@@ -10,18 +10,20 @@ function EnCokOkunanlar({ maxCount = 3 }) {
     return (
         <div>
             {populerHaberler.map((haber, index) => (
+                <>
                 <div className="encok-okunanlar row mb-3" key={haber.haber_id}>
                     <div className="d-flex align-items-center">
                         <img src={haber.haber_resim} alt={haber.haber_baslik} className="me-3"/>
                         <div id={`encokokunan-${haber.haber_id}`}>
                             <Link to={`/haberler/${haber.haber_id}`} className="text-white text-decoration-none">
                                 <h5 className="mb-0">{haber.haber_baslik}</h5>
-                                <small className="text-white">{haber.haber_} tarafından paylaşıldı!</small>
+                                <small className="text-white">{haber.haber_paylasan} tarafından paylaşıldı!</small>
                             </Link>
                         </div>
                     </div>
-                    {index < populerHaberler.length - 1 && <hr />}
                 </div>
+                {index < populerHaberler.length - 1 && <hr />}
+                </>
             ))}
         </div>
     );
